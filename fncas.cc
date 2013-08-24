1361fc0cd27f658eb61e35cf7f6a78f3ad2d100d
Dima Korolev <dima@publicverification.com>
2013-08-23 19:10:32 -0700

    Made parameter type generic.

--- fncas.cc
+++ fncas.cc
@@ -1,11 +1,19 @@
 #include <cstdio>
 #include <vector>
 
-double f(const std::vector<double>& x) {
+namespace fncas {
+
+template<typename T> struct output {};
+template<> struct output<std::vector<double> > { typedef double type; };
+
+}
+
+using namespace fncas;
+
+template<typename T> typename output<T>::type f(T x) {
   return x[0] * x[1] + x[2] - x[3];
 }
 
 int main() {
-  printf("%.3lf\n", f({5,8,9,7}));
+  printf("%.3lf\n", f(std::vector<double>({5,8,9,7})));
 }
-
