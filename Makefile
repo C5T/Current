976f60208d08683ec241e70e307c042a35ea2d3b
Dima Korolev <dima@publicverification.com>
2013-08-28 00:06:29 -0700

    Factored out the test.

--- Makefile
+++ Makefile
@@ -3,5 +3,9 @@ all: fncas
 fncas: fncas.cc
 	g++ --std=c++0x -o fncas fncas.cc
 
+test: test.cc
+	g++ --std=c++0x -o test test.cc
+	./test
+
 clean:
-	rm -f fncas
+	rm -f fncas test
--- test.cc
+++ test.cc
@@ -1,5 +1,3 @@
-// TODO(dkorolev): Readme, build instructions and a reference from this file.
-//
 // Please see fncas.h or Makefile for build instructions.
 
 #include <cassert>
@@ -10,14 +8,17 @@
 
 #include "fncas.h"
 
+#define BOOST_TEST_MODULE FNCAS
+#include <boost/test/included/unit_test.hpp>
+
 template<typename T> typename fncas::output<T>::type f(const T& x) {
   return 1 + x[0] * x[1] + x[2] - x[3] + 1;
 }
 
-int main() {
+BOOST_AUTO_TEST_CASE(SimpleTest) {
   std::vector<double> x({5,8,9,7});
-  printf("%lf\n", f(x));
+  BOOST_CHECK_EQUAL(f(x), 44);
   auto e = f(fncas::x(4));
-  printf("%s\n", e.debug_as_string().c_str());
-  printf("%lf\n", e.eval(x));
+  BOOST_CHECK_EQUAL(e.debug_as_string(), "((((1.000000+(x[0]*x[1]))+x[2])-x[3])+1.000000)");
+  BOOST_CHECK_EQUAL(e.eval(x), 44);
 }
