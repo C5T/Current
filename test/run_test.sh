2cef478c49e5512649ff68db8f20ccfd7e58b3cb
Dima Korolev <dima@publicverification.com>
2013-09-08 02:55:18 -0700

    HTML format.

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# TODO(dkorolev): HTML format.
+# TODO(dkorolev): Legend in HTML.
+# TODO(dkorolev): Actually compile with different flags.
 # TODO(dkorolev): Get rid of the Makefile and compile from this script with various options.
 
 MAX_ITERATIONS=2000000
@@ -25,8 +26,23 @@ done
 IFS="$SAVE_IFS"
 EOF
 
+echo '<!doctype html>'
+echo '<h1>Legend</h1>'
+echo 'TBD'
+echo '<h1>Header</h1>'
+echo '<table border=1 cellpadding=8>'
+echo -n '<tr>'
+echo -n '<td align=right>Function</td>'
+echo -n '<td align=right>Native, kQPS</td>'
+echo -n '<td align=right>Intermediate, kQPS</td>'
+echo -n '<td align=right>Compiled, kQPS</td>'
+echo -n '<td align=right>Intermediate / Native, %</td>'
+echo -n '<td align=right>Compiled / Native, %</td>'
+echo -n '<td align=right>Compiled / Intermediate, times</td>'
+echo -n '<td align=right>Compilation time, s</td>'
+echo '</tr>'
 for function in $(ls functions/ | cut -f1 -d.) ; do 
-  echo "Function: "$function
+  echo "Function: "$function >/dev/stderr
   data=''
   for action in generate evaluate intermediate_evaluate compiled_evaluate; do
     data+=$(./test_binary $function $action $MAX_ITERATIONS $MAX_SECONDS)':'
@@ -41,6 +57,16 @@ for function in $(ls functions/ | cut -f1 -d.) ; do
     eval_kqps=0.001/(1/gen_eval_qps - 1/gen_qps);
     eval_intermediate_kqps=0.001/(1/gen_eval_intermediate_qps - 1/gen_qps);
     eval_compiled_kqps=0.001/(1/gen_eval_compiled_qps - 1/gen_qps);
-    printf ("%20s EvalNative %10.2f kqps, EvalSlow %10.2f kqps, EvalFast %10.2f kqps, %.2fs compilation time.\n", name, eval_kqps, eval_intermediate_kqps, eval_compiled_kqps, compile_time);
+    printf ("<tr>");
+    printf ("<td align=right>%s</td>", name);
+    printf ("<td align=right>%10.2f kqps</td>\n", eval_kqps);
+    printf ("<td align=right>%10.2f kqps</td>\n", eval_intermediate_kqps);
+    printf ("<td align=right>%10.2f kqps</td>\n", eval_compiled_kqps);
+    printf ("<td align=right>%.0f%%</td>\n", 100 * eval_intermediate_kqps / eval_kqps);
+    printf ("<td align=right>%.0f%%</td>\n", 100 * eval_compiled_kqps / eval_kqps);
+    printf ("<td align=right>%.1fx</td>\n", eval_compiled_kqps / eval_intermediate_kqps);
+    printf ("<td align=right>%.2fs</td></tr>\n", compile_time);
+    printf ("</tr>");
   }'
 done
+echo '</table>'
