c7a768c6bca9fc5d4b7902bdb76ef07a54cd5107
Dima <dmitry.korolev@gmail.com>
2014-12-05 20:55:36 -1000

    Added 3party/gtest into Bricks.

---
+++ 3party/gtest/gtest-main.h
@@ -0,0 +1,16 @@
+// This file added by dkorolev.
+
+#ifndef THIRDPARTY_GTEST_MAIN_H
+#define THIRDPARTY_GTEST_MAIN_H
+
+#include "gtest.h"
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  // Postpone the `Death tests use fork(), which is unsafe particularly in a threaded context.` warning.
+  // Via https://code.google.com/p/googletest/wiki/AdvancedGuide#Death_Test_Styles
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  return RUN_ALL_TESTS();
+}
+
+#endif  // THIRDPARTY_GTEST_MAIN_H
---
+++ 3party/gtest/gtest.h
@@ -0,0 +1,8 @@
+// This file added by dkorolev.
+
+#ifndef THIRDPARTY_GTEST_H
+#define THIRDPARTY_GTEST_H
+
+#include "src/gtest-all.cc"
+
+#endif  // THIRDPARTY_GTEST_H
