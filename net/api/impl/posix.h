604494d14d26c361815cba7866896793b54b99b0
Dima <dmitry.korolev@gmail.com>
2014-12-12 15:59:05 -1000

    Minor cleanup.

--- net/api/impl/posix.h
+++ net/api/impl/posix.h
@@ -22,40 +22,23 @@ namespace bricks {
 namespace net {
 namespace api {
 
-struct HTTPClientPOSIX final {
-  // Request parameters.
-  string method_ = "";
-  string url_requested_ = "";
-  enum { None = 0, BodyFromBuffer, BodyFromFile } request_body_type_;
-  string request_body_content_type = "";
-  string request_body_contents = "";   // If request_body_type_ == BodyFromBuffer.
-  string request_body_file_name = "";  // If request_body_type_ == BodyFromFile.
-  enum { DestinationBuffer = 0, DestinationFile } destination_type;
-  string destination_file_name;  // If destination_type == DestinationFile.
-  unique_ptr<ScopedFileCleanup> file_cleanup_helper;
-  string user_agent = "";
-
-  // Output parameters.
-  int code_ = -1;
-  string url_after_redirects_ = "";
-
+class HTTPClientPOSIX final {
+ private:
   struct HTTPRedirectHelper : HTTPDefaultHelper {
     string location = "";
     inline void OnHeader(const char* key, const char* value) {
       if (string("Location") == key) {
         location = value;
-      } else {
-        //        std::cerr << "DIMA: " << key << " = " << value << "\n"; //if (string
       }
     }
   };
   typedef TemplatedHTTPReceivedMessage<HTTPRedirectHelper> HTTPRedirectableReceivedMessage;
-  std::unique_ptr<HTTPRedirectableReceivedMessage> message;
 
+ public:
   // The actual implementation.
   bool Go() {
-    url_after_redirects_ = url_requested_;
-    URLParser parsed_url(url_requested_);
+    response_url_after_redirects_ = request_url_;
+    URLParser parsed_url(request_url_);
     set<string> all_urls;
     bool redirected;
     do {
@@ -65,86 +48,96 @@ struct HTTPClientPOSIX final {
       }
       all_urls.insert(parsed_url.ComposeURL());
       Connection connection(Connection(ClientSocket(parsed_url.host, parsed_url.port)));
-      connection.BlockingWrite(method_ + ' ' + parsed_url.route + " HTTP/1.1\r\n");
+      connection.BlockingWrite(request_method_ + ' ' + parsed_url.route + " HTTP/1.1\r\n");
       connection.BlockingWrite("Host: " + parsed_url.host + "\r\n");
-      if (!user_agent.empty()) {
-        connection.BlockingWrite("User-Agent: " + user_agent + "\r\n");
+      if (!request_user_agent_.empty()) {
+        connection.BlockingWrite("User-Agent: " + request_user_agent_ + "\r\n");
       }
-      if (request_body_type_ == BodyFromBuffer) {
-        if (!request_body_content_type.empty()) {
-          connection.BlockingWrite("Content-Type: " + request_body_content_type + "\r\n");
-        }
-        connection.BlockingWrite("Content-Length: " + to_string(request_body_contents.length()) + "\r\n");
+      if (!request_body_content_type_.empty()) {
+        connection.BlockingWrite("Content-Type: " + request_body_content_type_ + "\r\n");
       }
+      connection.BlockingWrite("Content-Length: " + to_string(request_body_contents_.length()) + "\r\n");
       connection.BlockingWrite("\r\n");
-      if (request_body_type_ == BodyFromBuffer) {
-        connection.BlockingWrite(request_body_contents);
-      }
+      connection.BlockingWrite(request_body_contents_);
       // Attention! Achtung!
       // Calling SendEOF() (which is ::shutdown(socket, SHUT_WR);) results in slowly sent data
       // not being received. Tested on local and remote data with "chunked" transfer encoding.
       // Don't uncomment the next line!
       // connection.SendEOF();
-      message.reset(new HTTPRedirectableReceivedMessage(connection));
-      code_ = atoi(message->URL().c_str());  // TODO(dkorolev): Rename URL() to a more meaningful thing.
-      if (code_ >= 300 && code_ <= 399 && !message->location.empty()) {
+      message_.reset(new HTTPRedirectableReceivedMessage(connection));
+      response_code_ =
+          atoi(message_->URL().c_str());  // TODO(dkorolev): Rename URL() to a more meaningful thing.
+      if (response_code_ >= 300 && response_code_ <= 399 && !message_->location.empty()) {
         // TODO(dkorolev): Open at least one manual page about redirects before merging this code.
         redirected = true;
-        parsed_url = URLParser(message->location, parsed_url);
-        url_after_redirects_ = parsed_url.ComposeURL();
+        parsed_url = URLParser(message_->location, parsed_url);
+        response_url_after_redirects_ = parsed_url.ComposeURL();
       }
     } while (redirected);
     return true;
   }
+
+  const HTTPRedirectableReceivedMessage& GetMessage() const {
+    return *message_.get();
+  }
+
+ public:
+  // Request parameters.
+  string request_method_ = "";
+  string request_url_ = "";
+  string request_body_content_type_ = "";
+  string request_body_contents_ = "";
+  string request_user_agent_ = "";
+
+  // Output parameters.
+  int response_code_ = -1;
+  string response_url_after_redirects_ = "";
+
+ private:
+  std::unique_ptr<HTTPRedirectableReceivedMessage> message_;
 };
 
 template <>
 struct ImplWrapper<HTTPClientPOSIX> {
   inline static void PrepareInput(const HTTPRequestGET& request, HTTPClientPOSIX& client) {
-    client.method_ = "GET";
-    client.url_requested_ = request.url;
+    client.request_method_ = "GET";
+    client.request_url_ = request.url;
     if (!request.custom_user_agent.empty()) {
-      client.user_agent = request.custom_user_agent;
+      client.request_user_agent_ = request.custom_user_agent;
     }
   }
 
   inline static void PrepareInput(const HTTPRequestPOST& request, HTTPClientPOSIX& client) {
-    client.method_ = "POST";
-    client.url_requested_ = request.url;
+    client.request_method_ = "POST";
+    client.request_url_ = request.url;
     if (!request.custom_user_agent.empty()) {
-      client.user_agent = request.custom_user_agent;
+      client.request_user_agent_ = request.custom_user_agent;
     }
-    client.request_body_type_ = HTTPClientPOSIX::BodyFromBuffer;
-    client.request_body_contents = request.body;
-    client.request_body_content_type = request.content_type;
+    client.request_body_contents_ = request.body;
+    client.request_body_content_type_ = request.content_type;
   }
 
   inline static void PrepareInput(const HTTPRequestPOSTFromFile& request, HTTPClientPOSIX& client) {
-    client.method_ = "POST";
-    client.url_requested_ = request.url;
+    client.request_method_ = "POST";
+    client.request_url_ = request.url;
     if (!request.custom_user_agent.empty()) {
-      client.user_agent = request.custom_user_agent;
+      client.request_user_agent_ = request.custom_user_agent;
     }
-    client.request_body_type_ = HTTPClientPOSIX::BodyFromFile;
     try {
-      client.request_body_type_ = HTTPClientPOSIX::BodyFromBuffer;
-      client.request_body_contents = ReadFileAsString(request.file_name);
-      client.request_body_content_type = request.content_type;
+      client.request_body_contents_ = ReadFileAsString(request.file_name);
+      client.request_body_content_type_ = request.content_type;
     } catch (FileException&) {
       // TODO(dkorolev): Unfix this "fix" once we use proper exceptions in other clients (Apple, Android).
       throw HTTPClientException();
     }
-    client.request_body_content_type = request.content_type;
+    client.request_body_content_type_ = request.content_type;
   }
 
-  inline static void PrepareInput(const KeepResponseInMemory&, HTTPClientPOSIX& client) {
-    client.destination_type = HTTPClientPOSIX::DestinationBuffer;
+  inline static void PrepareInput(const KeepResponseInMemory&, HTTPClientPOSIX&) {
   }
 
-  inline static void PrepareInput(const SaveResponseToFile& save_to_file_request, HTTPClientPOSIX& client) {
+  inline static void PrepareInput(const SaveResponseToFile& save_to_file_request, HTTPClientPOSIX&) {
     assert(!save_to_file_request.file_name.empty());
-    client.destination_type = HTTPClientPOSIX::DestinationFile;
-    client.destination_file_name = save_to_file_request.file_name;
   }
 
   template <typename T_REQUEST_PARAMS, typename T_RESPONSE_PARAMS>
@@ -152,12 +145,12 @@ struct ImplWrapper<HTTPClientPOSIX> {
                                  const T_RESPONSE_PARAMS& /*response_params*/,
                                  const HTTPClientPOSIX& response,
                                  HTTPResponse& output) {
-    if (request_params.url != response.url_requested_) {
+    if (request_params.url != response.request_url_) {
       throw HTTPClientException();
     }
     output.url = request_params.url;
-    output.code = response.code_;
-    output.url_after_redirects = response.url_after_redirects_;
+    output.code = response.response_code_;
+    output.url_after_redirects = response.response_url_after_redirects_;
   }
 
   template <typename T_REQUEST_PARAMS, typename T_RESPONSE_PARAMS>
@@ -166,9 +159,8 @@ struct ImplWrapper<HTTPClientPOSIX> {
                                  const HTTPClientPOSIX& response,
                                  HTTPResponseWithBuffer& output) {
     ParseOutput(request_params, response_params, response, static_cast<HTTPResponse&>(output));
-    assert(response.destination_type == HTTPClientPOSIX::DestinationBuffer);
-    const auto& message = response.message;
-    output.body = message->HasBody() ? message->Body() : "";
+    const auto& message = response.GetMessage();
+    output.body = message.HasBody() ? message.Body() : "";
   }
 
   template <typename T_REQUEST_PARAMS, typename T_RESPONSE_PARAMS>
@@ -177,10 +169,9 @@ struct ImplWrapper<HTTPClientPOSIX> {
                                  const HTTPClientPOSIX& response,
                                  HTTPResponseWithResultingFileName& output) {
     ParseOutput(request_params, response_params, response, static_cast<HTTPResponse&>(output));
-    assert(response.destination_type == HTTPClientPOSIX::DestinationFile);
     // TODO(dkorolev): This is doubly inefficient. Gotta write the buffer or write chunks.
-    const auto& message = response.message;
-    WriteStringToFile(response_params.file_name, message->HasBody() ? message->Body() : "");
+    const auto& message = response.GetMessage();
+    WriteStringToFile(response_params.file_name, message.HasBody() ? message.Body() : "");
     output.body_file_name = response_params.file_name;
   }
 };
