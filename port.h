de182e3e49163254680739df9ae8fe3a31578d22
Dima <dmitry.korolev@gmail.com>
2014-12-13 01:53:31 -1000

    Removed CURL from the list.

--- port.h
+++ port.h
@@ -1,6 +1,6 @@
 // Cross-platform portability header.
 //
-// Ensures that one and only one of BRICKS_{POSIX,APPLE,CURL,ANDROID} is defined.
+// Ensures that one and only one of BRICKS_{POSIX,APPLE,ANDROID} is defined.
 // Keeps the one provided externally. Defaults to environmental setting is none has been defined.
 
 #ifndef BRICKS_PORT_H
@@ -12,7 +12,7 @@
 #error "`COUNT` should not be defined for port.h"
 #endif
 
-#define COUNT (defined(BRICKS_POSIX) + defined(BRICKS_APPLE) + defined(BRICKS_CURL) + defined(BRICKS_ANDROID))
+#define COUNT (defined(BRICKS_POSIX) + defined(BRICKS_APPLE) + defined(BRICKS_ANDROID))
 
 #if COUNT > 1
 #error "More than one `BRICKS_*` architectures have been defined."
