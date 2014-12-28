9a85e19cf486f66987a489f52c013a25e1a174b5
Dima <dmitry.korolev@gmail.com>
2014-12-27 18:08:55 -1000

    Bash tweaks.

--- scripts/check-all-headers.sh
+++ scripts/check-all-headers.sh
@@ -6,16 +6,15 @@
 
 set -u -e
 
-DIR=$(dirname $(readlink -e "$BASH_SOURCE"))
+SCRIPT_DIR=$(dirname $(readlink -e "$BASH_SOURCE"))
+RUN_DIR=$PWD
 
 echo -e "\033[1m\033[34mTesting all headers to comply with KnowSheet header-only paradigm.\033[0m"
 
 for i in $(for i in $(find . -iname "*.h" | grep -v 3party) ; do dirname $i ; done | sort -u) ; do
   echo -e "\033[1mDirectory\033[0m: $i"
-  if ! (cd $i; $DIR/check-headers.sh) ; then
-    echo -e "\033[1m\033[31mTerminating.\033[0m"
-    exit 1
-  fi
+  cd $i && $SCRIPT_DIR/check-headers.sh && cd $RUN_DIR && continue
+  echo -e "\033[1m\033[31mTerminating.\033[0m" && exit 1
 done
 
 echo -e "\033[1m\033[32mConfirmed\033[34m:\033[1m All headers comply with KnowSheet header-only paradigm.\033[0m"
