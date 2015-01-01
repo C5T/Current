da4ca7429fc036533b447f9dc31327876ae51cc9
Dima <dmitry.korolev@gmail.com>
2015-01-01 00:56:20 -1000

    Bricks/file 100% coverage.

--- file/file.h
+++ file/file.h
@@ -23,9 +23,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *******************************************************************************/
 
-// TODO(dkorolev): Add unit tests.
-// TODO(dkorolev): Move everything under bricks::file::FileSystem and have all the tests pass.
-
 #ifndef BRICKS_FILE_FILE_H
 #define BRICKS_FILE_FILE_H
 
@@ -35,6 +32,7 @@ SOFTWARE.
 
 #include <dirent.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #include "exceptions.h"
 
@@ -57,7 +55,7 @@ struct FileSystem {
       if (fi.read(&buffer[0], size).good()) {
         return buffer;
       } else {
-        throw FileException();
+        throw FileException();  // LCOV_EXCL_LINE: This line not unit tested.
       }
     } catch (const std::ifstream::failure&) {
       throw FileException();
@@ -80,8 +78,10 @@ struct FileSystem {
     }
   }
 
-  static std::string JoinPath(const std::string& path_name, const std::string& base_name) {
-    if (path_name.empty()) {
+  static inline std::string JoinPath(const std::string& path_name, const std::string& base_name) {
+    if (base_name.empty()) {
+      throw FileException();
+    } else if (path_name.empty() || base_name.front() == '/') {
       return base_name;
     } else if (path_name.back() == '/') {
       return path_name + base_name;
@@ -90,55 +90,66 @@ struct FileSystem {
     }
   }
 
-  static uint64_t GetFileSize(const std::string& file_name) {
+  static inline uint64_t GetFileSize(const std::string& file_name) {
     struct stat info;
     if (stat(file_name.c_str(), &info)) {
-      // TODO(dkorolev): Throw an exception and analyze errno.
-      return 0;
+      throw FileException();
     } else {
-      return static_cast<uint64_t>(info.st_size);
+      if (S_ISDIR(info.st_mode)) {
+        throw FileException();
+      } else {
+        return static_cast<uint64_t>(info.st_size);
+      }
     }
   }
 
-  static void CreateDirectory(const std::string& directory) {
+  static inline void CreateDirectory(const std::string& directory) {
     // Hard-code default permissions to avoid cross-platform compatibility issues.
-    ::mkdir(directory.c_str(), 0755);
-    // TODO(dkorolev): Throw an exception and analyze errno.
+    if (::mkdir(directory.c_str(), 0755)) {
+      // TODO(dkorolev): Analyze errno.
+      throw FileException();
+    }
   }
 
-  static void RenameFile(const std::string& old_name, const std::string& new_name) {
+  static inline void RenameFile(const std::string& old_name, const std::string& new_name) {
     if (::rename(old_name.c_str(), new_name.c_str())) {
-      // TODO(dkorolev): Throw an exception and analyze errno.
+      // TODO(dkorolev): Analyze errno.
+      throw FileException();
     }
   }
 
   // TODO(dkorolev): Make OutputFile not as tightly coupled with std::ofstream as it is now.
   typedef std::ofstream OutputFile;
 
-  static void ScanDirUntil(const std::string& directory, std::function<bool(const std::string&)> lambda) {
+  template <typename F>
+  static inline void ScanDirUntil(const std::string& directory, F&& f) {
     DIR* dir = ::opendir(directory.c_str());
     const auto closedir_guard = MakeScopeGuard([dir]() { ::closedir(dir); });
     if (dir) {
       while (struct dirent* entry = ::readdir(dir)) {
         if (*entry->d_name && ::strcmp(entry->d_name, ".") && ::strcmp(entry->d_name, "..")) {
-          if (!lambda(entry->d_name)) {
+          if (!f(entry->d_name)) {
             return;
           }
         }
       }
+    } else {
+      // TODO(dkorolev): Analyze errno.
+      throw FileException();
     }
   }
 
-  static void ScanDir(const std::string& directory, std::function<void(const std::string&)> lambda) {
-    ScanDirUntil(directory, [lambda](const std::string& filename) {
-      lambda(filename);
+  template <typename F>
+  static inline void ScanDir(const std::string& directory, F&& f) {
+    ScanDirUntil(directory, [&f](const std::string& filename) {
+      f(filename);
       return true;
     });
   }
 
   enum class RemoveFileParameters { ThrowExceptionOnError, Silent };
-  static void RemoveFile(const std::string& file_name,
-                         RemoveFileParameters parameters = RemoveFileParameters::ThrowExceptionOnError) {
+  static inline void RemoveFile(const std::string& file_name,
+                                RemoveFileParameters parameters = RemoveFileParameters::ThrowExceptionOnError) {
     if (::remove(file_name.c_str())) {
       if (parameters == RemoveFileParameters::ThrowExceptionOnError) {
         throw FileException();
@@ -159,6 +170,18 @@ struct FileSystem {
    private:
     std::string file_name_;
   };
+
+  enum class RemoveDirectoryParameters { ThrowExceptionOnError, Silent };
+  static inline void RemoveDirectory(
+      const std::string& directory,
+      RemoveDirectoryParameters parameters = RemoveDirectoryParameters::ThrowExceptionOnError) {
+    if (::rmdir(directory.c_str())) {
+      if (parameters == RemoveDirectoryParameters::ThrowExceptionOnError) {
+        // TODO(dkorolev): Analyze errno.
+        throw FileException();
+      }
+    }
+  }
 };
 
 }  // namespace bricks
