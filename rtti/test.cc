88743a15b6f852de5a584e24d142bc3c227fa183
Dima <dmitry.korolev@gmail.com>
2015-01-02 13:06:57 -1000

    Removed a taken care of TODO.

--- rtti/test.cc
+++ rtti/test.cc
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *******************************************************************************/
 
-// TODO(dkorolev): Add a test that throws UnrecognizedPolymorphicTypeException.
-
 #include "dispatcher.h"
 
 #include <string>
