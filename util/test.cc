61f4d1ea875f7c3a6912362a066ced5aba95f481
Dima <dmitry.korolev@gmail.com>
2015-01-02 13:15:24 -1000

    Added a few tests.

--- util/test.cc
+++ util/test.cc
@@ -25,6 +25,7 @@ SOFTWARE.
 // TODO(dkorolev): Test ScopeGuard and MakeScopeGuard as well.
 
 #include "util.h"
+#include "make_scope_guard.h"
 
 #include "../3party/gtest/gtest.h"
 #include "../3party/gtest/gtest-main.h"
@@ -37,4 +38,120 @@ TEST(Util, CompileTimeStringLength) {
   EXPECT_EQ(3u, bricks::CompileTimeStringLength(local_string));
   EXPECT_EQ(4u, bricks::CompileTimeStringLength(local_static_string));
   EXPECT_EQ(5u, bricks::CompileTimeStringLength(global_string));
+}
+
+TEST(Util, MakeScopeGuard) {
+  struct Object {
+    Object(std::string& story) : story_(story) { story_ += "constructed\n"; }
+    ~Object() { story_ += "destructed\n"; }
+    std::string& story_;
+  };
+
+  {
+    std::string story;
+    {
+      Object object(story);
+      EXPECT_EQ("constructed\n", story);
+    }
+    EXPECT_EQ("constructed\ndestructed\n", story);
+  }
+
+  {
+    std::string story = "lambda_begin\n";
+    EXPECT_EQ("lambda_begin\n", story);
+    {
+      EXPECT_EQ("lambda_begin\n", story);
+      const auto guard = bricks::MakeScopeGuard([&story]() { story += "lambda_end\n"; });
+      EXPECT_EQ("lambda_begin\n", story);
+    }
+    EXPECT_EQ("lambda_begin\nlambda_end\n", story);
+  }
+
+  {
+    std::string story = "helper_begin\n";
+    EXPECT_EQ("helper_begin\n", story);
+    struct Helper {
+      Helper(std::string& story) : story_(story), called_(false) {}
+      Helper(const Helper&) : story_(dummy_string_) {
+        assert(false);  // LCOV_EXCL_LINE
+      }
+      void operator=(const Helper&) {
+        assert(false);  // LCOV_EXCL_LINE
+      }
+      void operator()() {
+        if (!called_) {
+          story_ += "helper_end\n";
+          called_ = true;
+        } else {
+          assert(false);  // LCOV_EXCL_LINE
+        }
+      }
+      std::string& story_;
+      std::string dummy_string_;
+      bool called_;
+    };
+    Helper helper(story);
+    {
+      EXPECT_EQ("helper_begin\n", story);
+      const auto guard = bricks::MakeScopeGuard(helper);
+      EXPECT_EQ("helper_begin\n", story);
+      EXPECT_FALSE(helper.called_);
+    }
+    EXPECT_EQ("helper_begin\nhelper_end\n", story);
+    EXPECT_TRUE(helper.called_);
+  }
+}
+
+TEST(Util, MakePointerScopeGuard) {
+  struct Instance {
+    Instance(std::string& story) : story_(story) { story_ += "constructed\n"; }
+    ~Instance() { story_ += "destructed\n"; }
+    std::string& story_;
+  };
+
+  {
+    std::string story = "object\n";
+    EXPECT_EQ("object\n", story);
+    {
+      Instance instance(story);
+      EXPECT_EQ("object\nconstructed\n", story);
+    }
+    EXPECT_EQ("object\nconstructed\ndestructed\n", story);
+  }
+
+  {
+    std::string story = "pointer\n";
+    EXPECT_EQ("pointer\n", story);
+    {
+      Instance* pointer = new Instance(story);
+      EXPECT_EQ("pointer\nconstructed\n", story);
+      delete pointer;
+    }
+    EXPECT_EQ("pointer\nconstructed\ndestructed\n", story);
+  }
+
+  {
+    std::string story = "guarded_pointer\n";
+    EXPECT_EQ("guarded_pointer\n", story);
+    {
+      Instance* pointer = new Instance(story);
+      const auto guard = bricks::MakePointerScopeGuard(pointer);
+      EXPECT_EQ("guarded_pointer\nconstructed\n", story);
+    }
+    EXPECT_EQ("guarded_pointer\nconstructed\ndestructed\n", story);
+  }
+
+  {
+    std::string story = "custom_guarded_pointer\n";
+    EXPECT_EQ("custom_guarded_pointer\n", story);
+    {
+      Instance* pointer = new Instance(story);
+      const auto guard = bricks::MakePointerScopeGuard(pointer, [&story](Instance* p) {
+        story += "guarded_delete\n";
+        delete p;
+      });
+      EXPECT_EQ("custom_guarded_pointer\nconstructed\n", story);
+    }
+    EXPECT_EQ("custom_guarded_pointer\nconstructed\nguarded_delete\ndestructed\n", story);
+  }
 }
