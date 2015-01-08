fbc93e8a10720a62d4b71a0688b59623f68d5de4
Dima <dmitry.korolev@gmail.com>
2015-01-07 16:12:16 -1000

    CR: s/methos/method/.

--- net/api/impl/posix.h
+++ net/api/impl/posix.h
@@ -93,7 +93,7 @@ class HTTPClientPOSIX final {
       // HTTP request  message is: `GET /path HTTP/1.1`, "/path" is the second component of it.
       // HTTP response message is: `HTTP/1.1 200 OK`, "200" is the second component of it.
       // Thus, since the same code is used for request and response parsing as of now,
-      // the numerical response code "200" can be accessed with the same methos as the "/path".
+      // the numerical response code "200" can be accessed with the same method as the "/path".
       const int response_code_as_int = atoi(message_->Path().c_str());
       response_code_ = static_cast<HTTPResponseCode>(response_code_as_int);
       if (response_code_as_int >= 300 && response_code_as_int <= 399 && !message_->location.empty()) {
