06ca44816f44760a8aae8a05ff9c598a0dabfdda
Dima <dmitry.korolev@gmail.com>
2015-02-05 19:14:53 -0800

    CR: Multiple headers.

--- net/http/test.cc
+++ net/http/test.cc
@@ -197,26 +197,28 @@ TEST(PosixHTTPServerTest, SmokeChunkedResponse) {
 }
 
 TEST(PosixHTTPServerTest, SmokeWithHeaders) {
-  thread t([](Socket s) {
-             HTTPServerConnection c(s.Accept());
-             EXPECT_EQ("GET", c.HTTPRequest().Method());
-             EXPECT_EQ("/header", c.HTTPRequest().RawPath());
-             c.SendHTTPResponse("OK", HTTPResponseCode::OK, c.HTTPRequest().Body(), {{"foo", "bar"}});
-           },
-           Socket(FLAGS_net_http_test_port));
+  thread
+  t([](Socket s) {
+      HTTPServerConnection c(s.Accept());
+      EXPECT_EQ("GET", c.HTTPRequest().Method());
+      EXPECT_EQ("/header", c.HTTPRequest().RawPath());
+      c.SendHTTPResponse("OK", HTTPResponseCode::OK, c.HTTPRequest().Body(), {{"foo", "bar"}, {"baz", "meh"}});
+    },
+    Socket(FLAGS_net_http_test_port));
   Connection connection(ClientSocket("localhost", FLAGS_net_http_test_port));
   connection.BlockingWrite("GET /header HTTP/1.1\r\n");
   connection.BlockingWrite("Host: localhost\r\n");
-  connection.BlockingWrite("Content-Length: 3\r\n");
+  connection.BlockingWrite("Content-Length: 19\r\n");
   connection.BlockingWrite("\r\n");
-  connection.BlockingWrite("meh");
+  connection.BlockingWrite("custom_content_type");
   connection.BlockingWrite("\r\n");
   connection.SendEOF();
   t.join();
   EXPECT_EQ(
       "HTTP/1.1 200 OK\r\n"
-      "Content-Type: meh\r\n"
+      "Content-Type: custom_content_type\r\n"
       "foo: bar\r\n"
+      "baz: meh\r\n"
       "Content-Length: 2\r\n"
       "\r\n"
       "OK",
