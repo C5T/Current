b2427e0e60ecfb5611a6adcb4c84e754d84f26d4
Dima <dmitry.korolev@gmail.com>
2015-02-22 19:25:28 -0800

    More exception types for ScanDir().

--- file/exceptions.h
+++ file/exceptions.h
@@ -34,6 +34,14 @@ struct FileException : Exception {
   FileException() : Exception() {}
 };
 
+struct DirDoesNotExistException : FileException {
+  DirDoesNotExistException() : FileException() {}
+};
+
+struct PathIsNotADirectoryException : FileException {
+  PathIsNotADirectoryException() : FileException() {}
+};
+
 }  // namespace bricks
 
 #endif  // BRICKS_FILE_EXCEPTIONS_H
