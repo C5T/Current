fae7c5fc31ff6b7bc6cf558127257df7b54d6bb7
msft-dkorolev <dmitry.korolev+msft@gmail.com>
2015-03-04 14:07:48 -0800

    strings::{Trim,ToLower,ToUpper}().

--- strings/util.h
+++ strings/util.h
@@ -27,6 +27,10 @@ SOFTWARE.
 
 #include <cstddef>
 #include <type_traits>
+#include <cstring>
+#include <cctype>
+#include <algorithm>
+#include <iterator>
 
 // These are small headers, and #include "bricks/strings/util.h" should be enough.
 #include "printf.h"
@@ -37,6 +41,7 @@ SOFTWARE.
 namespace bricks {
 namespace strings {
 
+// Keep the lowercase name to possibly act as a replacement for `std::to_string`.
 template <typename T>
 inline std::string to_string(T&& something) {
   return std::to_string(something);
@@ -47,9 +52,65 @@ inline std::string to_string<std::string>(std::string&& shame_std_to_string_does
   return shame_std_to_string_does_not_support_string;
 }
 
+// Use camel-case `ToString()` within Bricks.
+template <typename T>
+inline std::string ToString(T&& something) {
+  return bricks::strings::to_string(something);
+}
+
 template <size_t N>
 constexpr typename std::enable_if<(N > 0), size_t>::type CompileTimeStringLength(char const (&)[N]) {
   return N - 1;
+}
+
+inline std::string Trim(const char* input, const size_t length) {
+  const char* begin = input;
+  const char* end = input + length;
+  const char* output_begin = begin;
+  while (output_begin < end && ::isspace(*output_begin)) {
+    ++output_begin;
+  }
+  const char* output_end = end;
+  while (output_end > output_begin && ::isspace(*(output_end - 1))) {
+    --output_end;
+  }
+  return std::string(output_begin, output_end);
+}
+
+inline std::string Trim(const char* s) {
+  return Trim(s, ::strlen(s));
+}
+
+inline std::string Trim(const std::string& s) {
+  return Trim(s.c_str(), s.length());
+}
+
+template<typename T> inline std::string ToLower(T begin, T end) {
+  std::string result;
+  std::transform(begin, end, std::back_inserter(result), ::tolower);
+  return result;
+}
+
+inline std::string ToLower(const char* s) {
+  return ToLower(s, s + ::strlen(s));
+}
+
+template<typename T> inline std::string ToLower(const T& input) {
+  return ToLower(std::begin(input), std::end(input));
+}
+
+template<typename T> inline std::string ToUpper(T begin, T end) {
+  std::string result;
+  std::transform(begin, end, std::back_inserter(result), ::toupper);
+  return result;
+}
+
+inline std::string ToUpper(const char* s) {
+  return ToUpper(s, s + ::strlen(s));
+}
+
+template<typename T> inline std::string ToUpper(const T& input) {
+  return ToUpper(std::begin(input), std::end(input));
 }
 
 }  // namespace strings
