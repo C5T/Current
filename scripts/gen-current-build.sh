a6e4cec06bfc04fc6431365e22474c051d0f5c86
Dima <dmitry.korolev@gmail.com>
2016-05-09 02:31:11 -0700

    Richer `current_build.h`.

--- scripts/gen-current-build.sh
+++ scripts/gen-current-build.sh
@@ -9,6 +9,11 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
+EPOCH_SECONDS="$(date +'%s')"
+if [[ $? -ne 0 ]]; then
+	exit 1
+fi
+
 COMPILER_INFO="$($CPLUSPLUS --version 2>&1)"
 if [[ $? -ne 0 ]]; then
 	exit 1
@@ -30,20 +35,28 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
+GIT_DIFF_NAMES_MULTILINE="$(git diff --name-only)"
+GIT_DIFF_NAMES=${GIT_DIFF_NAMES_MULTILINE//$'\n'/\\n}
+
 GIT_DIFF_MD5SUM="$(git diff --no-ext-diff | md5sum)"
 if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
 cat >$1.tmp << EOF
+// clang-format off
+
 #ifndef CURRENT_BUILD_H
 #define CURRENT_BUILD_H
 
 #include "$SCRIPT_DIR/../port.h"
 
+#include <chrono>
+#include <vector>
 #include <string>
 
 #include "$SCRIPT_DIR/../TypeSystem/struct.h"
+#include "$SCRIPT_DIR/../Bricks/strings/split.h"
 
 namespace current {
 namespace build {
@@ -56,18 +69,51 @@ constexpr static const char* kCompilerFlags = "$CPPFLAGS";
 constexpr static const char* kLinkerFlags = "$LDFLAGS";
 constexpr static const char* kCompilerInfo = "$COMPILER_INFO";
 
+inline const std::vector<std::string>& GitDiffNames() {
+  static std::vector<std::string> result = current::strings::Split("$GIT_DIFF_NAMES", '\n');
+  return result;
+}
+
 CURRENT_STRUCT(Info) {
-  CURRENT_FIELD(date_time, std::string, __DATE__ ", " __TIME__);
-  CURRENT_FIELD(git_commit, std::string, kGitCommit);
+  CURRENT_FIELD(build_time, std::string, __DATE__ ", " __TIME__);
+  CURRENT_FIELD(build_dir, std::string, "$PWD");
+  CURRENT_FIELD(build_user, std::string, "$(whoami)");
+  CURRENT_FIELD(build_time_epoch_microseconds, std::chrono::microseconds, std::chrono::microseconds(1000ull * 1000ull * $EPOCH_SECONDS));
+  CURRENT_FIELD(git_commit_hash, std::string, kGitCommit);
+  CURRENT_FIELD(git_dirty_files, (std::vector<std::string>), GitDiffNames());
   CURRENT_FIELD(git_branch, std::string, kGitBranch);
   CURRENT_FIELD(os, std::string, kOS);
   CURRENT_FIELD(compiler, std::string, kCompiler);
   CURRENT_FIELD(compiler_flags, std::string, kCompilerFlags);
   CURRENT_FIELD(linker_flags, std::string, kLinkerFlags);
   CURRENT_FIELD(compiler_info, std::string, kCompilerInfo);
-  std::tuple<std::string, std::string, std::string, std::string, std::string, std::string, std::string, std::string>
+  std::tuple<
+    std::string,
+    std::string,
+    std::string,
+    std::chrono::microseconds,
+    std::string,
+    std::vector<std::string>,
+    std::string,
+    std::string,
+    std::string,
+    std::string,
+    std::string,
+    std::string>
   AsTuple() const {
-    return std::tie(date_time, git_commit, git_branch, os, compiler, compiler_flags, linker_flags, compiler_info);
+    return std::tie(
+      build_time,
+      build_dir,
+      build_user,
+      build_time_epoch_microseconds,
+      git_commit_hash,
+      git_dirty_files,
+      git_branch,
+      os,
+      compiler,
+      compiler_flags,
+      linker_flags,
+      compiler_info);
   }
   bool operator==(const Info& rhs) const {
     return AsTuple() == rhs.AsTuple();
@@ -82,8 +128,13 @@ CURRENT_STRUCT(Info) {
 
 #endif  // CURRENT_BUILD_H
 
+// clang-format on
+
 /*
 $ # Extra pieces of data to force \`make\` rebuild the code as repository state changes.
+
+$ git diff --name_only
+$GIT_DIFF_NAMES_MULTILINE
 
 $ git status
 $GIT_STATUS
