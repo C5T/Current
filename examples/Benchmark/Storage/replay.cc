8f9f718b8eb6afb5b980be4ec6b596b95c9b92c1
Maxim Zhurovich <zhurovich@gmail.com>
2016-07-15 18:47:02 +0300

    Added stream subscribers.

--- examples/Benchmark/Storage/replay.cc
+++ examples/Benchmark/Storage/replay.cc
@@ -1,6 +1,6 @@
-#include "../../../Bricks/dflags/dflags.h"
+#include <atomic>
 
-//#include "../../../Bricks/file/file.h"
+#include "../../../Bricks/dflags/dflags.h"
 
 #include "schema.h"
 
@@ -11,6 +11,7 @@ DEFINE_int16(sleep,
              10,
              "The time in milliseconds to sleep in the spin-lock while checking "
              "actual replayed entries count.");
+DEFINE_uint16(subs, 0, "The number of dummy stream subscribers.");
 
 inline void GenerateTestData(const std::string& file, uint32_t size) {
   current::FileSystem::RmFile(file, current::FileSystem::RmFileParameters::Silent);
@@ -23,8 +24,54 @@ inline void GenerateTestData(const std::string& file, uint32_t size) {
   }
 }
 
-inline void PerformReplayBenchmark(const std::string& file, std::chrono::milliseconds spin_lock_sleep) {
+template <typename T>
+struct RawLogSubscriberImpl {
+  struct MutationCollector {
+    void operator()(const EntryDictUpdated& e) { crc_ ^= current::CRC32(Value<Entry>(e.data).value); }
+
+    template <typename E>
+    void operator()(const E&) const {}
+
+    uint32_t crc_ = 0u;
+  };
+
+  using EntryResponse = current::ss::EntryResponse;
+  using TerminationResponse = current::ss::TerminationResponse;
+
+  RawLogSubscriberImpl() : entries_seen_(0u) {}
+
+  EntryResponse operator()(const T& transaction, idxts_t, idxts_t) {
+    for (const auto& m : transaction.mutations) {
+      m.Call(mutation_collector_);
+    }
+    ++entries_seen_;
+    return EntryResponse::More;
+  }
+
+  TerminationResponse Terminate() {
+    terminating_ = true;
+    return TerminationResponse::Terminate;
+  }
+
+  EntryResponse EntryResponseIfNoMorePassTypeFilter() const {
+    if (terminating_) {
+      return EntryResponse::Done;
+    } else {
+      return EntryResponse::More;
+    }
+  }
+
+  std::atomic<uint64_t> entries_seen_;
+  bool terminating_ = false;
+  MutationCollector mutation_collector_;
+};
+
+inline void PerformReplayBenchmark(const std::string& file,
+                                   std::chrono::milliseconds spin_lock_sleep,
+                                   uint16_t subscribers_count) {
   using stream_t = typename storage_t::persister_t::sherlock_t;
+  using transaction_t = typename stream_t::entry_t;
+  using subscriber_t = current::ss::StreamSubscriber<RawLogSubscriberImpl<transaction_t>, transaction_t>;
 
   // Owning storage replay.
   {
@@ -32,10 +79,22 @@ inline void PerformReplayBenchmark(const std::string& file, std::chrono::millise
     const auto begin = current::time::Now();
     stream_t stream(file);
     const auto stream_initialized = current::time::Now();
+    std::vector<subscriber_t> subscribers(subscribers_count);
+    std::vector<current::sherlock::SubscriberScope> sub_scopes;
+    if (subscribers_count) {
+      for (size_t i = 0u; i < subscribers_count; ++i) {
+        sub_scopes.emplace_back(std::move(stream.Subscribe(subscribers[i])));
+      }
+    }
+    const auto subscribers_created = current::time::Now();
     storage_t storage(stream);
     const auto end = current::time::Now();
     std::cout << "* Stream initialization: " << (stream_initialized - begin).count() / 1000 << " ms\n";
-    std::cout << "* Storage replay: " << (end - stream_initialized).count() / 1000 << " ms" << std::endl;
+    if (subscribers_count) {
+      std::cout << "* Spawning subscribers: " << (subscribers_created - stream_initialized).count() / 1000
+                << " ms\n";
+    }
+    std::cout << "* Storage replay: " << (end - subscribers_created).count() / 1000 << " ms" << std::endl;
   }
 
   struct PublisherAcquirer {
@@ -53,6 +112,14 @@ inline void PerformReplayBenchmark(const std::string& file, std::chrono::millise
     stream.MovePublisherTo(acquirer);
     const uint64_t stream_size = stream.InternalExposePersister().Size();
     const auto stream_initialized = current::time::Now();
+    std::vector<subscriber_t> subscribers(subscribers_count);
+    std::vector<current::sherlock::SubscriberScope> sub_scopes;
+    if (subscribers_count) {
+      for (size_t i = 0u; i < subscribers_count; ++i) {
+        sub_scopes.emplace_back(std::move(stream.Subscribe(subscribers[i])));
+      }
+    }
+    const auto subscribers_created = current::time::Now();
     uint64_t storage_size = 0u;
     storage_t storage(stream);
     // Spin lock checking the number of imported entries.
@@ -64,7 +131,11 @@ inline void PerformReplayBenchmark(const std::string& file, std::chrono::millise
     }
     const auto end = current::time::Now();
     std::cout << "* Stream initialization: " << (stream_initialized - begin).count() / 1000 << " ms\n";
-    std::cout << "* Storage replay: " << (end - stream_initialized).count() / 1000 << " ms" << std::endl;
+    if (subscribers_count) {
+      std::cout << "* Spawning subscribers: " << (subscribers_created - stream_initialized).count() / 1000
+                << " ms\n";
+    }
+    std::cout << "* Storage replay: " << (end - subscribers_created).count() / 1000 << " ms" << std::endl;
   }
 }
 
@@ -73,6 +144,6 @@ int main(int argc, char** argv) {
   if (FLAGS_gen) {
     GenerateTestData(FLAGS_file, FLAGS_size);
   } else {
-    PerformReplayBenchmark(FLAGS_file, std::chrono::milliseconds(FLAGS_sleep));
+    PerformReplayBenchmark(FLAGS_file, std::chrono::milliseconds(FLAGS_sleep), FLAGS_subs);
   }
 }
