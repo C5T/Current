65619a0d684f4c2631300ed352b3dd3f2258e533
Maxim Zhurovich <zhurovich@gmail.com>
2016-08-16 18:41:04 +0300

    `-pthread` warning fix for OS X.

--- regression_tests/type_system/test.sh
+++ regression_tests/type_system/test.sh
@@ -22,7 +22,10 @@ TEST_LIST=(
 
 CPLUSPLUS=${CPLUSPLUS:-g++}
 CPPFLAGS=${CPPFLAGS:- -std=c++11 -ftemplate-backtrace-limit=0  -ftemplate-depth=10000}
-LDFLAGS=${LDFLAGS:- -pthread}
+OS=$(uname)
+if [[ $OS -ne "Darwin" ]]; then
+	LDFLAGS=${LDFLAGS:- -pthread}
+fi
 
 mkdir -p .current
 
