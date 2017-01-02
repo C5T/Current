444cf513d3bf571c2f77452c2d529429d81a016a
Dima <dmitry.korolev@gmail.com>
2017-01-02 14:29:01 -0800

    CR.

--- examples/Iris/step1_analyze_tsv.sh
+++ examples/Iris/step1_analyze_tsv.sh
@@ -3,5 +3,5 @@
 # Step 1: Analyze the TSV file, generate its description, JSON representation, and the schema header.
 
 (cd data &&
- rm -f rm -f dataset.description dataset.json dataset.h &&
+ rm -f dataset.description dataset.json dataset.h &&
  ../../../Utils/JSONSchema/analyze_tsv.sh dataset.tsv) && echo OK
