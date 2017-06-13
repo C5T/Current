2abeeebaad3488b27dd920dda0acef45d0450471
Ivan Babak <babak.john@gmail.com>
2017-06-13 12:59:29 -0700

    Blocks/HTTP/impl/posix_client: Added URLs to the `HTTPRedirectLoopException` message.

--- Blocks/HTTP/impl/posix_client.h
+++ Blocks/HTTP/impl/posix_client.h
@@ -80,7 +80,7 @@ class GenericHTTPClientPOSIX final {
       redirected = false;
       const std::string composed_url = parsed_url.ComposeURL();
       if (all_urls.count(composed_url)) {
-        CURRENT_THROW(current::net::HTTPRedirectLoopException());
+        CURRENT_THROW(current::net::HTTPRedirectLoopException(current::strings::Join(all_urls, ' ') + ' ' + composed_url));
       }
       all_urls.insert(composed_url);
       current::net::Connection connection(
