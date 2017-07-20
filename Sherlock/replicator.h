b2304bdbbe8cac87118734fede0a181fef269392
Maxim Zhurovich <zhurovich@gmail.com>
2017-07-20 21:08:26 +0300

    Added `GetNumberOfEntries` for remote stream.

--- Sherlock/replicator.h
+++ Sherlock/replicator.h
@@ -2,6 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2016 Grigory Nikolaenko <nikolaenko.grigory@gmail.com>
+          (c) 2017 Maxim Zhurovich <zhurovich@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -64,6 +65,15 @@ class SubscribableRemoteStream final {
         if (remote_schema != schema_) {
           CURRENT_THROW(RemoteStreamInvalidSchemaException());
         }
+      } else {
+        CURRENT_THROW(RemoteStreamDoesNotRespondException());
+      }
+    }
+
+    uint64_t GetNumberOfEntries() const {
+      const auto response = HTTP(GET(url_ + "?sizeonly"));
+      if (response.code == HTTPResponseCode.OK) {
+        return FromString<uint64_t>(response.body);
       } else {
         CURRENT_THROW(RemoteStreamDoesNotRespondException());
       }
@@ -265,6 +275,10 @@ class SubscribableRemoteStream final {
     } catch (const current::sync::InDestructingModeException&) {
       CURRENT_THROW(StreamInGracefulShutdownException());
     }
+  }
+
+  uint64_t GetNumberOfEntries() const {
+    return stream_.ObjectAccessorDespitePossiblyDestructing().GetNumberOfEntries();
   }
 
  private:
