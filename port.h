adfa27d6ae44235d125bf8153a9f64bb38456531
Dima <dmitry.korolev@gmail.com>
2017-11-26 13:27:47 -0800

    A dummy revert to trigger Travis.

--- port.h
+++ port.h
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *******************************************************************************/
 
-// blah
-
 // Cross-platform portability header.
 //
 // Ensures that one and only one of CURRENT_{POSIX,APPLE,ANDROID} is defined.
