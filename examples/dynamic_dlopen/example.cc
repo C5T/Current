674a90fa7fa3cdcb3a809acd92805b38564fea20
Dima <dmitry.korolev@gmail.com>
2018-05-07 07:21:11 -0700

    A real example using the dlopen wrapper.

---
+++ examples/dynamic_dlopen/example.cc
@@ -0,0 +1,14 @@
+#include "impl.h"
+
+#include "../../blocks/http/api.h"
+#include "../../bricks/dflags/dflags.h"
+
+DEFINE_string(input_filename, "../iris/data/dataset.json", "The input Irises dataset.");
+DEFINE_uint16(port, 3000, "The port to serve on.");
+
+int main(int argc, char** argv) {
+  ParseDFlags(&argc, &argv);
+  DynamicDLOpenIrisExampleImpl impl(FLAGS_input_filename, FLAGS_port);
+  std::cout << "Working on " << impl.TotalFlowers() << " Iris flowers." << std::endl;
+  HTTP(FLAGS_port).Join();
+}
