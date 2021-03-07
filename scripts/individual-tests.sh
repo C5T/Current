e79d32b87ea45ecfae375360cf92edf358e636e7
Dima <dmitry.korolev@gmail.com>
2021-03-07 19:29:53 +0500

    Added parallel test runs to `scripts/individual-tests.sh` and the top-level `Makefile`.

--- scripts/individual-tests.sh
+++ scripts/individual-tests.sh
@@ -17,12 +17,12 @@ for i in $(find . -name test.cc | sort) ; do
   MD5=$(echo $DIR | md5sum)
   HASH=$((0x${MD5%% *}))
   SHARD=$((((HASH % DESIRED_MOD) + DESIRED_MOD) % DESIRED_MOD))
-  echo -e "\n\033[0m\033[1mDir\033[0m: \033[36m${DIR}\033[0m"
+  if [[ $DESIRED_MOD == 1 ]]; then
+    echo -e "\n\033[0m\033[1mDir\033[0m: \033[36m${DIR}\033[0m"
+  fi
   if [[ " ${EXCLUDE[@]} " =~ " $DIR " ]]; then
     echo "Excluded."
-  elif [[ $SHARD != $DESIRED_SHARD ]]; then
-    echo "In another shard."
-  else
+  elif [[ $SHARD == $DESIRED_SHARD ]]; then
     cd $DIR
     if ! make -s test ; then
       FAILURES="$FAILURES\n- $DIR"
@@ -31,18 +31,22 @@ for i in $(find . -name test.cc | sort) ; do
   fi
 done
 
-# And a dedicated run for the `dlopen()`-based compilation that involves a symlink to the very Current.
-  echo -e "\n\033[0m\033[1mDir\033[0m: \033[36m./bricks/system\033[0m [with --current_base_dir_for_dlopen_test set]"
-( \
-  DIR=$(./scripts/fullpath.sh $PWD) && \
-  cd bricks/system && \
-  make .current/test > /dev/null \
-  && ./.current/test --current_base_dir_for_dlopen_test=$DIR \
-) || FAILURES="$FAILURES\n- bricks/system [with --current_base_dir_for_dlopen_test set]"
+if [[ $DESIRED_SHARD == 0 ]]; then
+  # And a dedicated run for the `dlopen()`-based compilation that involves a symlink to the very Current.
+    echo -e "\n\033[0m\033[1mDir\033[0m: \033[36m./bricks/system\033[0m [with --current_base_dir_for_dlopen_test set]"
+  ( \
+    DIR=$(./scripts/fullpath.sh $PWD) && \
+    cd bricks/system && \
+    make .current/test > /dev/null \
+    && ./.current/test --current_base_dir_for_dlopen_test=$DIR \
+  ) || FAILURES="$FAILURES\n- bricks/system [with --current_base_dir_for_dlopen_test set]"
+fi
 
 if [ "$FAILURES" = "" ] ; then
- echo -e "\n\033[32m\033[1mALL TESTS PASS.\033[0m"
- exit 0
+  if [[ $DESIRED_MOD == 1 ]]; then
+    echo -e "\n\033[32m\033[1mALL TESTS PASS.\033[0m"
+  fi
+  exit 0
 else
  echo -e "\n\033[31m\033[1mFAIL\033[0m"
  echo -e "Failed tests:$FAILURES"
