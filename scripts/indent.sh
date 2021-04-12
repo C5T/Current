11f9b1dcc6681b0b8cf273fe08584713b102f510
Dima <dmitry.korolev@gmail.com>
2021-04-12 09:22:15 +0500

    Added `-type f` to not `clang-format` symlinked files.

--- scripts/indent.sh
+++ scripts/indent.sh
@@ -4,7 +4,7 @@
 # according to .clang-format in this directory or above.
 
 CLANG_FORMAT=${1:-clang-format-10}
-(find . -name "*.cc" ; find . -name "*.h") \
+(find . -type f -name "*.cc" ; find . -type f -name "*.h") \
 | grep -v "/.current/" | grep -v "/3rdparty/" \
 | grep -v "/docu_" \
 | xargs "$CLANG_FORMAT" -i
