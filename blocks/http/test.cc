d9fdb9744574e1659c79ceab2bcc08e134c80dc5
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2023-02-11 01:45:10 -0800

    Added POSTing a `vector` and a `map`, breaks the test.

--- blocks/http/test.cc
+++ blocks/http/test.cc
@@ -1319,6 +1319,64 @@ TEST(HTTPAPI, PatchRequestWithJSON) {
   EXPECT_EQ(200, static_cast<int>(response.code));
 }
 
+TEST(HTTPAPI, POSTRequestWithVector) {
+  auto reserved_port = current::net::ReserveLocalPort();
+  const int port = reserved_port;
+  auto& http_server = HTTP(std::move(reserved_port));
+
+  const auto scope = http_server.Register("/post_with_vector", [](Request r) {
+    EXPECT_EQ("POST", r.method);
+    EXPECT_EQ("[101,102,103]", r.body);
+    ASSERT_TRUE(r.headers.Has("Content-Type"));
+    EXPECT_EQ("application/json; charset=utf-8", r.headers.Get("Content-Type"));
+    r("POST with JSON Array OK.");
+  });
+  std::vector<int> v{101,102,103};
+  const auto response = HTTP(POST(Printf("http://localhost:%d/post_with_vector", port), v));
+  EXPECT_EQ("POST with JSON Array OK.", response.body);
+  EXPECT_EQ(200, static_cast<int>(response.code));
+}
+
+TEST(HTTPAPI, POSTRequestWithMapStringInt) {
+  auto reserved_port = current::net::ReserveLocalPort();
+  const int port = reserved_port;
+  auto& http_server = HTTP(std::move(reserved_port));
+
+  const auto scope = http_server.Register("/post_with_map", [](Request r) {
+    EXPECT_EQ("POST", r.method);
+    EXPECT_EQ("{\"bar\":2,\"foo\":1}", r.body);
+    ASSERT_TRUE(r.headers.Has("Content-Type"));
+    EXPECT_EQ("application/json; charset=utf-8", r.headers.Get("Content-Type"));
+    r("POST with JSON MapStringInt OK.");
+  });
+  std::map<std::string, int32_t> m;
+  m["foo"] = 1;
+  m["bar"] = 2;
+  const auto response = HTTP(POST(Printf("http://localhost:%d/post_with_map", port), m));
+  EXPECT_EQ("POST with JSON MapStringInt OK.", response.body);
+  EXPECT_EQ(200, static_cast<int>(response.code));
+}
+
+TEST(HTTPAPI, POSTRequestWithMapIntString) {
+  auto reserved_port = current::net::ReserveLocalPort();
+  const int port = reserved_port;
+  auto& http_server = HTTP(std::move(reserved_port));
+
+  const auto scope = http_server.Register("/post_with_map", [](Request r) {
+    EXPECT_EQ("POST", r.method);
+    EXPECT_EQ("[[0,\"the\"],[42,\"answer\"]]", r.body);  // TODO(dkorolev): Maybe revisit `map<non-string, T>` JSONs?
+    ASSERT_TRUE(r.headers.Has("Content-Type"));
+    EXPECT_EQ("application/json; charset=utf-8", r.headers.Get("Content-Type"));
+    r("POST with JSON MapIntString OK.");
+  });
+  std::map<int32_t, std::string> m;
+  m[42] = "answer";
+  m[0] = "the";
+  const auto response = HTTP(POST(Printf("http://localhost:%d/post_with_map", port), m));
+  EXPECT_EQ("POST with JSON MapIntString OK.", response.body);
+  EXPECT_EQ(200, static_cast<int>(response.code));
+}
+
 TEST(HTTPAPI, UserAgent) {
   auto reserved_port = current::net::ReserveLocalPort();
   const int port = reserved_port;
