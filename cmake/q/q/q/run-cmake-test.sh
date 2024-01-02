c08e357913f29eb64a6cbd4ab06ffefb01ef1ee9
Dima <dima@current.ai>
2024-01-02 17:50:17 +0800

    ... more macOS experiments.

---
+++ cmake/q/q/q/run-cmake-test.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+#
+# This script illustrates how to use the CMakeLists.txt-based build of project that use `current`.
+# It is meant to be run from within an empty directory.
+
+set -e
+
+touch .gitignore
+touch golden_gitignore
+
+if ! diff golden_gitignore .gitignore ; then
+  echo "Diff failed on line ${LINENO}."
+  exit 1
+fi
+
+if ! [ -s Makefile ] ; then
+  echo 'Need the `Makefile`, `curl`-ing one.'
+  curl -s https://raw.githubusercontent.com/dimacurrentai/Current/cmake/cmake/Makefile >Makefile
+	# TODO(dkorolev): Fix the repo & branch on the line above once merged.
+fi
+
+mkdir -p src
+cat >src/hw.cc <<EOF
+#include <iostream>
+int main() {
+#ifdef NDEBUG
+  std::cout << "Hello, World! NDEBUG=1." << std::endl;
+#else
+  std::cout << "Hello, World! NDEBUG is unset." << std::endl;
+#endif
+}
+EOF
+
+# This runs `cmake .` for Release mode, which is output into `.current`.
+echo "::group::configure"
+make .current
+echo "::endgroup::"
+
+# The run of `cmake .` must have cloned `current` and `googletest` and added them into `.gitignore`.
+if ! [ -d current ] || ! [ -d googletest ] ; then
+  echo 'Either `current` or `googletest` were not cloned.'
+  exit 1
+fi
+
+echo 'current/' >>golden_gitignore
+echo 'googletest/' >>golden_gitignore
+if ! diff golden_gitignore .gitignore ; then
+  echo "Diff failed on line ${LINENO}."
+  exit 1
+fi
+
+echo "::group::make [release]"
+make
+echo "::endgroup::"
+
+echo '.current/' >>golden_gitignore
+if ! diff golden_gitignore .gitignore ; then
+  echo "Diff failed on line ${LINENO}."
+  exit 1
+fi
+
+echo "::group::.current/hw"
+.current/hw
+echo "::endgroup::"
+
+echo "::group::make debug"
+make debug
+echo "::endgroup::"
+
+echo '.current_debug/' >>golden_gitignore
+diff golden_gitignore .gitignore || (echo 'Wrong `.gitignore`, exiting.'; exit 1)
+
+echo "::group::.current_debug/hw"
+.current_debug/hw
+echo "::endgroup::"
+
+cat >src/lib_add.cc <<EOF
+#include "lib_add.h"
+int lib_add(int a, int b) {
+  return a + b;
+}
+EOF
+
+cat >src/lib_add.h <<EOF
+#pragma once
+int lib_add(int a, int b);
+EOF
+
+cat >src/test_gtest.cc <<EOF
+#include <gtest/gtest.h>  // IWYU pragma: keep
+#include "lib_add.h"
+TEST(SmokeGoogletest, TwoTimesTwo) {
+  EXPECT_EQ(4, 2 * 2);
+}
+TEST(SmokeGoogletest, TwoPlusThree) {
+  EXPECT_EQ(5, lib_add(2, 3));
+}
+EOF
+
+cat >src/test_current_gtest.cc <<EOF
+#include "3rdparty/gtest/gtest-main.h"  // IWYU pragma: keep
+#include "lib_add.h"
+TEST(SmokeCurrentGoogletest, TwoTimesTwo) {
+  EXPECT_EQ(4, 2 * 2);
+}
+TEST(SmokeCurrentGoogletest, TwoPlusThree) {
+  EXPECT_EQ(5, lib_add(2, 3));
+}
+EOF
+
+echo
+
+echo "::group::release_test"
+make test
+echo "::endgroup::"
+
+echo "::group::debug_test"
+make debug_test
+echo "::endgroup::"
+
+echo
+
+touch src/test_gtest.cc
+T0_GTEST=$(date +%s)
+echo "::group::one line change google gtest release"
+make test
+echo "::endgroup::"
+T1_GTEST=$(date +%s)
+
+touch src/test_current_gtest.cc
+T0_CURRENT_GTEST=$(date +%s)
+echo "::group::one line change current gtest release"
+make test
+echo "::endgroup::"
+T1_CURRENT_GTEST=$(date +%s)
+
+touch src/test_gtest.cc
+T0_DEBUG_GTEST=$(date +%s)
+echo "::group::one line change google gtest debug"
+make debug_test
+echo "::endgroup::"
+T1_DEBUG_GTEST=$(date +%s)
+
+touch src/test_current_gtest.cc
+T0_DEBUG_CURRENT_GTEST=$(date +%s)
+echo "::group::one line change current gtest debug"
+make debug_test
+echo "::endgroup::"
+T1_DEBUG_CURRENT_GTEST=$(date +%s)
+
+echo
+echo "One-line change time, Current gtest, debug: $((T1_DEBUG_CURRENT_GTEST - T0_DEBUG_CURRENT_GTEST))s"
+echo "One-line change time, Current gtest, release: $((T1_CURRENT_GTEST - T0_CURRENT_GTEST))s"
+echo
+echo "One-line change time, Google gtest, debug: $((T1_DEBUG_GTEST - T0_DEBUG_GTEST))s"
+echo "One-line change time, Google gtest, release: $((T1_GTEST - T0_GTEST))s"
+echo
+echo '(The numbers for `Current gtest` should be worse, as Current is header-only.)'
+echo
+
+for i in ./.current/test_gtest ./.current/test_current_gtest ./.current_debug/test_gtest ./.current_debug/test_current_gtest ; do
+  echo "::group::test output for $i"
+  $i
+  echo "::endgroup::"
+done
+
+echo
+
+cat >src/so_mul.cc <<EOF
+extern "C" int so_mul(int a, int b) {
+  return a * b;
+}
+EOF
+
+cat >src/call_so.cc <<EOF
+#include <iostream>
+#include "bricks/dflags/dflags.h"
+#include "bricks/system/syscalls.h"
+DEFINE_string(so, "", "The path to the `.so` library to use.");
+int main(int argc, char** argv) {
+  ParseDFlags(&argc, &argv);
+  if (FLAGS_so.empty()) {
+    std::cout << "Must set `--so`." << std::endl;
+    return 1;
+  }
+  auto dl = current::bricks::system::DynamicLibrary(FLAGS_so);
+  auto pf = dl.template Get<int (*)(int, int)>("so_mul");
+  if (!pf) {
+    std::cout << "Must set `--so`." << std::endl;
+    return 1;
+  }
+  auto f = *pf;
+  int result = f(2, 3);
+  if (result == 6) {
+    std::cout << "OK, 2 * 3 == 6." << std::endl;
+  } else {
+    std::cout << "WA, 2 * 3 != " << result << '.' << std::endl;
+    return 1;
+  }
+}
+EOF
+
+# This forces `cmake` re-configuration, for both Debug and Release builds.
+# TODO(dkorolev): This should probably happen automatically if the set of files under `src/` has changed!
+touch CMakeLists.txt
+
+echo "::group::build .current/libso_mul.so and .current/call_so"
+make
+echo "::endgroup::"
+
+echo "::group::build .current_debug/libso_mul.so and .current_debug/call_so"
+make debug
+echo "::endgroup::"
+
+echo "::group::call .so-defined functions from manually built binaries, debug <=> release"
+if [ "$(uname)" == "Darwin" ] ; then
+  SO_EXT=dylib
+else
+  SO_EXT=so
+fi
+echo "Using '.${SO_EXT}' extension for shared libraries."
+./.current/call_so --so .current/libso_mul.${SO_EXT}
+./.current/call_so --so .current_debug/libso_mul.${SO_EXT}
+./.current_debug/call_so --so .current_debug/libso_mul.${SO_EXT}
+./.current_debug/call_so --so .current/libso_mul.${SO_EXT}
+echo "::endgroup::"
